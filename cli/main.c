63c0479dfd9868efeaff478977e2d635857093f4
hypothermic <admin@hypothermic.nl>
2019-11-03 20:38:00 +0100

    Parse format from argument
    Signed-off-by: hypothermic <admin@hypothermic.nl>

--- cli/main.c
+++ cli/main.c
@@ -1,18 +1,19 @@
 #include <glib.h>
 
 #include "../core/archive.h"
+#include "../core/mode.h"
 
-static gchar** input_file_array;
-static gchar*  output_file;
-static gchar*  archive_format;
+static gchar** arg_input_file_array;
+static gchar*  arg_output_file;
+static gchar*  arg_archive_format;
 
 static const GOptionEntry command_entries[] = {
-        {"input",       'i', G_OPTION_FLAG_NONE, G_OPTION_ARG_FILENAME_ARRAY, &input_file_array,
+        {"input",       'i', G_OPTION_FLAG_NONE, G_OPTION_ARG_FILENAME_ARRAY, &arg_input_file_array,
                 "The files that will be copied into the output archive", NULL},
-        {"output",      'o', G_OPTION_FLAG_NONE, G_OPTION_ARG_FILENAME,       &output_file,
+        {"output",      'o', G_OPTION_FLAG_NONE, G_OPTION_ARG_FILENAME,       &arg_output_file,
                 "Where to store the output archive", NULL},
 
-        {"format",      'f', G_OPTION_FLAG_NONE, G_OPTION_ARG_STRING,         &archive_format,
+        {"format",      'f', G_OPTION_FLAG_NONE, G_OPTION_ARG_STRING,         &arg_archive_format,
                 "Which archive format to use", NULL},
 
         {NULL}
@@ -21,6 +22,7 @@ static const GOptionEntry command_entries[] = {
 int main(int argc, char **argv) {
     GError *error = NULL;
     GOptionContext *option_context;
+    uc_archive_t format = _UC_ARCHIVE_MAX;
 
     g_print("\nUltimateCompressor CLI v%s build %s\n", UC_API_VERSION, UC_BUILD_VERSION);
 
@@ -34,22 +36,38 @@ int main(int argc, char **argv) {
     g_option_context_free(option_context);
 
     // Check args
-    if (!input_file_array) {
+    if (!arg_input_file_array) {
         g_printerr("%s: %s \n", "Error while parsing arguments", "Input files not specified");
         return 2;
     }
-    if (!output_file) {
+    if (!arg_output_file) {
         g_printerr("%s: %s \n", "Error while parsing arguments", "Output path not specified");
         return 3;
     }
-    if (!archive_format) {
+    if (!arg_archive_format) {
         g_printerr("%s: %s \n", "Error while parsing arguments", "Archive format not specified");
         return 4;
     }
 
-
-
     FOR_EACH_ARCHIVE {
-        g_print("Archive format %s is %s\n", uc_archive_to_str(&archive), uc_archive_get_func_is_supported(&archive)() ? "enabled" : "disabled");
+        if (strcmp(uc_archive_to_str(&archive), arg_archive_format) == 0) {
+            if (uc_archive_get_func_is_supported(&archive)()) {
+                format = archive;
+            } else {
+                g_printerr("%s: %s %s \n", "Error while parsing arguments", uc_archive_to_str(&archive), "archive format is not supported");
+                return 5;
+            }
+        }
     }
+
+    if (!format || format == _UC_ARCHIVE_MAX) {
+        g_printerr("%s: %s \"%s\"\n", "Error while parsing arguments", "No format matching", arg_archive_format);
+        return 6;
+    }
+
+    // For good measure...
+    g_error_free(error);
+    g_strfreev(arg_input_file_array);
+    g_free(arg_output_file);
+    g_free(arg_archive_format);
 }
